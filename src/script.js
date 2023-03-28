acd256aafaca1265dff797d20979d1affacbe976
Yummy Fran Palomares <93894214+YummyFran@users.noreply.github.com>
2023-03-29 07:48:57 +0800

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,30 +1,7 @@
 window.addEventListener("scroll", () => {
     const nav = document.querySelector(".navigation")
-    const card = document.querySelectorAll(".card")
-    
+    const card = document.querySelector(".card")
+
     nav.classList.toggle("show-nav", window.scrollY > window.innerHeight)
-    card.forEach(el => el.classList.toggle("show-card", window.scrollY > 400))
+    card.classList.toggle("show-card", window.scrollY > 400)
 })
-
-// let typed = new Typed("#name", {
-//     strings: ["Yummy Fran", "Crismark"],
-//     typeSpeed: 50,
-//     backSpeed: 50,
-//     loop: true
-// })
-
-// let header = document.getElementById("home")
-// let changeBackground = () => {
-//     setTimeout(() => {
-//         header.style.backgroundImage = `url('../assets/crismark.jpg')`
-//         console.log("1st")
-    
-//         setTimeout(() => {
-//             header.style.backgroundImage = `url('../assets/banner.jpg')`
-//             console.log("2nd")
-//             requestAnimationFrame(changeBackground)
-//         }, 2050)
-//     }, 2350)
-// }
-
-// requestAnimationFrame(changeBackground)
