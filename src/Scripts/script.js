d24b2dcff9de77d48be5e589ead8df6e49d3c0ef
Yummy Fran Palomares <93894214+YummyFran@users.noreply.github.com>
2023-04-03 14:58:34 +0800

    Resume is up!

--- src/Scripts/script.js
+++ src/Scripts/script.js
@@ -9,24 +9,26 @@ const contact = document.querySelector(".contact-container")
 const clickables = document.getElementsByClassName('pointer')
 const cursor = document.querySelector('.cursor')
 
-window.addEventListener("scroll", () => {   //onScroll Event
+if(nav)  //if wala ni exist ang nav(sa resume page) then stop na ang code diri
+
+window.addEventListener("scroll", () => {   //onScroll Event --> add ra ug class inigscroll sa specific pageHeight
     nav.classList.toggle("show-nav", window.scrollY > window.innerHeight)
     card.forEach(el => el.classList.toggle("show-card", window.scrollY > 400))
     project.classList.toggle("show-project", window.scrollY > (window.innerHeight + 400))
     contact.classList.toggle("show-contact", window.scrollY > (2 * window.innerHeight + 400))
 })
 
-document.addEventListener('mousemove', e => {   //onMouseMove Event
+document.addEventListener('mousemove', e => {   //onMouseMove Event --> custom cursor
     cursor.style.left = e.clientX + 'px'
     cursor.style.top = e.clientY + 'px'
 })
 
-document.addEventListener('click', e => { //onClick Event
+document.addEventListener('click', e => { //onClick Event --> click animation
     cursor.classList.add('clicked')
-    setTimeout(() => cursor.classList.remove('clicked'), 600)
+    setTimeout(() => cursor.classList.remove('clicked'), 200)
 })
 
-for(let i = 0; i < clickables.length; i++) {    //onHover(React) on onmouseover/onmouseout(Vanilla)
+for(let i = 0; i < clickables.length; i++) {    //onHover(React) on onmouseover/onmouseout(Vanilla) --> change color sa cursor
     clickables[i].addEventListener('mouseover', () => cursor.classList.add('hovering'))
     clickables[i].addEventListener('mouseout', () => cursor.classList.remove('hovering'))
 }
